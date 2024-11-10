e5e77ab9200d9561c07ebf0ce40527cfbfc92187
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-11-10 13:56:33 -0500

    added filecount and printing how many files were moved

--- core/sorter.py
+++ core/sorter.py
@@ -5,12 +5,18 @@ from core.utils import clean_path
 def sort():
   settings = get_settings()
   downloads = os.listdir(settings['downloads'])
-
+  file_count = 0
+  
   for file in downloads:
     file_extension = os.path.splitext(file)[1].lower()
 
     if file_extension in settings['destinations']:
+      file_count += 1
       file_path = clean_path(f'{settings['downloads']}/{file}')
       destination_path = settings['destinations'][file_extension]
 
       os.system(f'mv {file_path} {destination_path}')
+  if file_count >= 0:
+    print(f'Moved {file_count} files!')
+  else:
+    print('No files were moved!')
