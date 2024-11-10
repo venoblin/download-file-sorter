7a0cd6a231aa1ad885987f1243adc5ded582865c
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-11-09 22:58:55 -0500

    worked on sorter

--- core/sorter.py
+++ core/sorter.py
@@ -3,8 +3,8 @@ from settings import get_settings
 
 def sort():
   settings = get_settings()
-  downloads = os.listdir(path)
+  downloads = os.listdir(settings['downloads'])
 
-  print(path)
+  print(downloads)
 
 sort()
