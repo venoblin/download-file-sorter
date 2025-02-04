237e896840c805bdd5fbf44b2b66257f0f53b96d
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-02-03 23:35:30 -0500

    changed f string to double quotes

--- core/sorter.py
+++ core/sorter.py
@@ -12,13 +12,13 @@ def sort():
 
     if file_extension in settings['destinations']:
       file_count += 1
-      file_path = clean_path(f'{settings['downloads']}/{file}')
+      file_path = clean_path(f"{settings['downloads']}/{file}")
       destination_path = settings['destinations'][file_extension]
 
-      os.system(f'mv {file_path} {destination_path}')
-      print(f'Moved: {file}')
+      os.system(f"mv {file_path} {destination_path}")
+      print(f"Moved: {file}")
       
   if file_count > 0:
-    print(f'Moved {file_count} files!')
+    print(f"Moved {file_count} files!")
   else:
     print('No files were moved!')
