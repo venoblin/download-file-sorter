d5189339adb435850f6ec663bf9eebb9e8581628
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-02-03 23:30:05 -0500

    changed to single quotes

--- core/utils.py
+++ core/utils.py
@@ -1,13 +1,13 @@
 def clean_path(path):
-  new_path = ""
+  new_path = ''
 
   for letter in path:    
-    if letter == " ":
-      new_path += "\\ "
-    elif letter == "(":
-      new_path += "\\("
-    elif letter == ")":
-      new_path += "\\)"
+    if letter == ' ':
+      new_path += '\\ '
+    elif letter == '(':
+      new_path += '\\('
+    elif letter == ')':
+      new_path += '\\)'
     else:
       new_path += letter
 
