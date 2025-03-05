71bacd9868bc1ff34f5a3d8981a3492c800f9b65
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-03-05 15:51:14 -0500

    added dir path to get settings so it loc

--- core/settings.py
+++ core/settings.py
@@ -1,7 +1,10 @@
+import os
 import json
 
 def get_settings():
-  with open('settings.json', 'r') as file:
+  dir_path = os.path.dirname(os.path.abspath(__file__))
+
+  with open(f'{dir_path}/../settings.json', 'r') as file:
     data = json.load(file)
 
   return data
